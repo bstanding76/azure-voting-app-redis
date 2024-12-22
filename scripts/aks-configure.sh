9322dfe4dd7f533c8b7aef7c69ec24c36708b7ac
Brendon Standing <brendon.standing@futuru.ai>
2024-12-22 21:21:52 +0000

    Demo - first commit

---
+++ scripts/aks-configure.sh
@@ -0,0 +1,12 @@
+# Create a resource group
+az group create --name rg-lbg-voteapp-test-uks --location ukwest
+
+# Create a container registry
+az acr create --resource-group rg-lbg-voteapp-test-uks --name crlbgvoteapptestuk --sku Basic
+
+# Create a Kubernetes cluster
+#az aks create \
+#    --resource-group rg-lbg-voteapp-test-uks \
+#    --name voting-app \
+#    --node-count 1 \
+#    --generate-ssh-keys
