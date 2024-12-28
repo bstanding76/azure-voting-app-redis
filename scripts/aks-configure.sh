1675d8a9648b2ca2866c60203c407bb97d661eff
Brendon Standing <brendon.standing@futuru.ai>
2024-12-28 07:09:47 +0000

    BS - Refactor from devops pipeline 43

--- scripts/aks-configure.sh
+++ scripts/aks-configure.sh
@@ -5,8 +5,8 @@ az group create --name rg-lbg-voteapp-test-uks --location ukwest
 az acr create --resource-group rg-lbg-voteapp-test-uks --name crlbgvoteapptestuk --sku Basic
 
 # Create a Kubernetes cluster
-#az aks create \
-#    --resource-group rg-lbg-voteapp-test-uks \
-#    --name voting-app \
-#    --node-count 1 \
-#    --generate-ssh-keys
+az aks create \
+    --resource-group rg-lbg-voteapp-test-uks \
+    --name demovotingapp \
+    --node-count 1 \
+    --generate-ssh-keys
